bff08b49dccd3ef5bfca8a274b4b83822882ef3f
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-09 00:51:04 +0530

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import logoImg from "../assets/images/logo.png"
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full px-20 flex items-center justify-between border-b-[0.031rem] border-b-[#ef1313] bg-[#1A1919] z-50">
+        <div className="left">
+          <img className="h-16" src={logoImg} alt="" />
+        </div>
+        <div className="center flex gap-10">
+          <h1 className="text-[1.4rem] font-light">Events</h1>
+          <h1 className="text-[1.4rem] font-light">Programs</h1>
+          <h1 className="text-[1.4rem] font-light">Community</h1>
+          <h1 className="text-[1.4rem] font-light">Ranks</h1>
+        </div>
+        <div className="right flex gap-10">
+          <button className="text-[1.2rem] hover:bg-white hover:text-black hover:border-0 transition-all duration-200 cursor-pointer px-4 py-0.5 rounded-md border">LOGIN</button>
+          <button className="text-[1.2rem] bg-[#FF4D4C] transition-all duration-200 cursor-pointer px-4 py-1 rounded-md hover:border hover:bg-transparent">REGISTER NOW</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,67 @@
+import Navbar from "../components/Navbar";
+import heroVideo from "../assets/videos/hero.mp4";
+
+const Hero = () => {
+  return (
+    <section className="relative h-screen w-full overflow-hidden">
+
+      <div className="relative z-50">
+        <Navbar />
+      </div>
+
+      <video
+        className="absolute inset-0 z-0 h-full w-full object-cover"
+        muted
+        autoPlay
+        loop
+        playsInline
+      >
+        <source src={heroVideo} type="video/mp4" />
+      </video>
+
+      <div className="absolute inset-0 z-10 bg-linear-to-r from-black via-black/85 via-45% to-transparent" />
+
+      <div className="absolute top-28 right-48 z-30 inline-flex items-center gap-2 rounded-md bg-[#161618]/95 px-4 py-1 text-xs font-semibold tracking-wide text-white border border-white/10 backdrop-blur-sm">
+        <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
+        </span>
+        <span className="text-[0.9rem] uppercase font-bold tracking-widest text-red-500">Live :</span>
+        <span className="text-[1rem] text-gray-300">Episode 14 - Bengaluru Regionals</span>
+        <span className="text-[1rem] font-semibold text-red-500 ml-2 cursor-pointer hover:text-red-400 transition-colors">WATCH LIVE</span>
+      </div>
+
+      <div className="relative z-20 flex h-full items-center px-14">
+        <div className="max-w-2xl">
+
+          <h1 className="text-7xl font-semibold tracking-wide text-white">
+            INDIA'S ULTIMATE
+            <br />
+            ROBOTICS ARENA
+          </h1>
+
+          <p className="mt-2 text-xl text-gray-300">
+            Build.Compete.Rank.The National
+            <br />
+            Ecosystem for Robotics Arena
+          </p>
+
+          <div className="mt-6 flex gap-3">
+
+            <button className="cursor-pointer rounded-lg border border-red-500 bg-red-500 px-5 py-3 text-sm font-medium text-white transition-all duration-300 hover:border-transparent hover:bg-red-600">
+              CREATE ACCOUNT
+            </button>
+
+            <button className="cursor-pointer rounded-lg border border-white bg-transparent px-5 py-3 text-sm font-medium text-white transition-all duration-300 hover:bg-white hover:text-black">
+              EXPLORE EVENTS
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default Hero;
